fb9cfb033b06da8a8371b1cdd8fd73d566ed5d14
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-03-18 22:52:02 -0400

    Logica do assinante

--- ros2_ws/src/my_py_pkg/my_py_pkg/smartphone.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/smartphone.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from example_interfaces.msg import String
+
 class SmartphoneNode(Node):
     def __init__(self):
        super().__init__("smartphone")
+       self.subscriber_ = self.create_subscription(String,"robot_news", self.callback_robot_news ,10)
+       self.get_logger().info("Smartphone has benn started")
+
+    def callback_robot_news(self,msg):
+        self.get_logger().info(msg.data)
 
 
 
